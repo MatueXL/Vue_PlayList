fe2f8862c124582a7f64e96596657293ec67a111
matuexl <matuexl@gmail.com>
2020-08-11 23:31:42 +0800

    键盘事件和键值修饰符

---
+++ 6.键盘事件和键值修饰符/app.js
@@ -0,0 +1,11 @@
+new Vue({
+    el: '#app',
+    methods: {
+        LogMyName() {
+            console.log('你正在输入名字...');
+        },
+        LogMyAge() {
+            console.log('你正在输入年龄...');
+        }
+    }
+});
