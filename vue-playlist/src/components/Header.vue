264168ac1399d73551d7f19f35acfc0b1f0a42f6
matuexl <matuexl@gmail.com>
2020-08-25 13:19:04 +0800

    实例的生命周期方法

--- vue-playlist/src/components/Header.vue
+++ vue-playlist/src/components/Header.vue
@@ -21,8 +21,32 @@ export default {
   methods: {
     changeParentTitle() {
       // this.parentTitle = 'changed!';
-      this.$emit('changeParentTitle','Change Parent Title By Children Component!');
+      this.$emit('changeParentTitle', 'Change Parent Title By Children Component!');
     }
+  },
+  beforeCreate() {
+    alert('组件实例化之前执行的函数');
+  },
+  created() {
+    alert('组件实例化完毕，但页面还未展示');
+  },
+  beforeMount() {
+    alert('组件挂载前，页面仍未展示，但虚拟dom已配置')
+  },
+  mounted() {
+    alert('组件挂载后，此方法执行后，展示页面')
+  },
+  beforeUpdate() {
+    alert('组件更新前，页面仍未更新，但虚拟dom已配置')
+  },
+  updated() {
+    alert('组件更新后，此方法执行后，展示页面');
+  },
+  beforedestory() {
+    alert('组件销毁前');
+  },
+  destroyed() {
+    alert('组件销毁后');
   }
 }
 </script>
